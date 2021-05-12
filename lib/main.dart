8ee70f438e525ade9f16176ad4b64ea07ec8538d
pranay5470 <50202506+pranay5470@users.noreply.github.com>
2021-05-12 09:42:56 +0530

    intial

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: App(),
+  ));
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
